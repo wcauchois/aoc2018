b7be828fcae228857b605d196338b39ff0d9c3bb
Bill Cauchois <wcauchois@gmail.com>
2018-12-01 00:06:12 -0500

    where we droppin boys (day 1)

---
+++ day1/main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+from itertools import  cycle
+
+def Input(filename):
+  with open(filename, 'r') as fp:
+    return [l.strip('\n') for l in fp.readlines()]
+
+nums = [int(x) for x in Input('input.txt')]
+#nums = [+7, +7, -2, -7, -4]
+seen = set()
+freq = 0
+for n in cycle(nums):
+  freq += n
+  if freq in seen:
+    print(freq)
+    break
+  seen.add(freq)
+
+#print(sum([int(x) for x in Input('input.txt')]))
+
